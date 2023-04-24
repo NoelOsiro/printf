985a2cc833c65cfa45f78f1af731a63c7ffe6852
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-04-24 22:34:20 +0000

    C

--- helpers.c
+++ helpers.c
@@ -1,75 +1,72 @@
+#include "main.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "main.h"
 
-static void write_string(const char* str, int* n) {
-    for (const char* c = str; *c; c++) {
-        buffer[*n] = *c;
-        (*n)++;
-        if (*n >= BUFFER_SIZE) {
-            // buffer full, write to stdout
-            write(STDOUT_FILENO, buffer, BUFFER_SIZE);
-            *n = 0;
-        }
-    }
-}
 
-static void write_hex(uint8_t c, int* n) {
-    buffer[*n] = '\\';
-    (*n)++;
+char buffer[BUFFER_SIZE];
+
+/**
+ * write_char - Helper function for the 'c' case
+ * @c: Character to write
+ * @n: Pointer to integer keeping track of number of characters written
+ *
+ * Description: Writes a single character to the buffer and flushes the buffer to stdout if it's full
+ */
+void write_char(char c, int *n) {
+
+    buffer[(*n)++] = (uint8_t)c;
+
     if (*n >= BUFFER_SIZE) {
-        // buffer full, write to stdout
-        write(STDOUT_FILENO, buffer, BUFFER_SIZE);
-        *n = 0;
-    }
-    buffer[*n] = 'x';
-    (*n)++;
-    if (*n >= BUFFER_SIZE) {
-        // buffer full, write to stdout
-        write(STDOUT_FILENO, buffer, BUFFER_SIZE);
-        *n = 0;
-    }
-    buffer[*n] = "0123456789ABCDEF"[c >> 4];
-    (*n)++;
-    if (*n >= BUFFER_SIZE) {
-        // buffer full, write to stdout
-        write(STDOUT_FILENO, buffer, BUFFER_SIZE);
-        *n = 0;
-    }
-    buffer[*n] = "0123456789ABCDEF"[c & 0x0F];
-    (*n)++;
-    if (*n >= BUFFER_SIZE) {
-        // buffer full, write to stdout
+        /* buffer full, write to stdout */  
         write(STDOUT_FILENO, buffer, BUFFER_SIZE);
         *n = 0;
     }
 }
 
-// Helper function for the 's' case
+
+/**
+ * write_string - Helper function for the 's' case
+ * @str: Null-terminated string to write
+ * @n: Pointer to integer keeping track of number of characters written
+ *
+ * Description: Writes a null-terminated string to the buffer and flushes the buffer to stdout if it's full
+ */
 void write_string(char *str, int *n) {
     while (*str) {
         buffer[(*n)++] = (uint8_t)(*str++);
         if (*n >= BUFFER_SIZE) {
-            // buffer full, write to stdout
+            /* buffer full, write to stdout */
             write(STDOUT_FILENO, buffer, BUFFER_SIZE);
             *n = 0;
         }
     }
 }
 
-// Helper function for the '%' case
+/**
+ * write_percent - Helper function for the '%' case
+ * @n: Pointer to integer keeping track of number of characters written
+ *
+ * Description: Writes a '%' character to the buffer and flushes the buffer to stdout if it's full
+ */
 void write_percent(int *n) {
     buffer[(*n)++] = '%';
     if (*n >= BUFFER_SIZE) {
-        // buffer full, write to stdout
+         /* buffer full, write to stdout */
         write(STDOUT_FILENO, buffer, BUFFER_SIZE);
         *n = 0;
     }
 }
 
-// Helper function for the 'd' and 'i' cases
+
+/**
+ * write_int - Helper function for the 'd' and 'i' cases
+ * @n: Pointer to integer keeping track of number of characters written
+ * @num: Integer to write
+ *
+ * Description: Writes an integer to the buffer and flushes the buffer to stdout if it's full
+ */
 void write_int(int arg, int *n) {
     if (arg < 0) {
         buffer[(*n)++] = '-';
@@ -80,13 +77,22 @@ void write_int(int arg, int *n) {
     }
     buffer[(*n)++] = (uint8_t)((arg % 10) + '0');
     if (*n >= BUFFER_SIZE) {
-        // buffer full, write to stdout
+         /* buffer full, write to stdout */
         write(STDOUT_FILENO, buffer, BUFFER_SIZE);
         *n = 0;
     }
 }
 
-// Helper function for the 'u', 'o', 'x', and 'X' cases
+/**
+ * write_uint - Helper function for the 'u', 'o', 'x', 'X', and 'p' cases
+ * @n: Pointer to integer keeping track of number of characters written
+ * @num: Unsigned integer to write
+ * @base: Base of the integer representation (8 for octal, 10 for decimal, 16 for hexadecimal)
+ * @is_upper: Boolean indicating whether to use uppercase letters for hexadecimal (0 for lowercase, 1 for uppercase)
+ * @prefix: Prefix to use for octal ('0'), hexadecimal ('0x' or '0X'), or NULL for decimal and pointer cases
+ *
+ * Description: Writes an unsigned integer to the buffer and flushes the buffer to stdout if it's full
+ */
 void write_uint(uint32_t arg, int *n, int base, int is_uppercase) {
     static const char *digits_lowercase = "0123456789abcdef";
     static const char *digits_uppercase = "0123456789ABCDEF";
@@ -96,13 +102,20 @@ void write_uint(uint32_t arg, int *n, int base, int is_uppercase) {
     }
     buffer[(*n)++] = (uint8_t)digits[arg % base];
     if (*n >= BUFFER_SIZE) {
-        // buffer full, write to stdout
+         /* buffer full, write to stdout */
         write(STDOUT_FILENO, buffer, BUFFER_SIZE);
         *n = 0;
     }
 }
 
-// Helper function for the 'S' case
+/**
+ * write_string_special - Helper function for the 'S' case
+ * @str: String to write
+ * @n: Pointer to integer keeping track of number of characters written
+ *
+ * Description: Writes a string to the buffer, replacing non-printable characters with their ASCII code value in hexadecimal
+ *              and flushes the buffer to stdout if it's full.
+ */
 void write_string_special(char *str, int *n) {
     while (*str) {
         if (*str < ' ' || *str >= 0x7f) {
@@ -114,15 +127,20 @@ void write_string_special(char *str, int *n) {
             buffer[(*n)++] = (uint8_t)(*str);
         }
         if (*n >= BUFFER_SIZE) {
-            // buffer full, write to stdout
+            /* buffer full, write to stdout */
             write(STDOUT_FILENO, buffer, BUFFER_SIZE);
             *n = 0;
         }
         str++;
     }
 }
-
-// Helper function for the 'p' case
+/**
+ * write_pointer - Helper function for the 'p' case
+ * @n: Pointer to integer keeping track of number of characters written
+ * @ptr: Pointer to write
+ *
+ * Description: Writes a pointer to the buffer in hexadecimal format and flushes the buffer to stdout if it's full.
+ */
 void write_pointer(void *arg, int *n) {
     write_uint((uint32_t)(intptr_t)arg, n, 16, 0);
 }
--- main.h
+++ main.h
@@ -5,7 +5,15 @@
 #include <stdint.h>
 
 #define BUFFER_SIZE 1024
+extern char buffer[BUFFER_SIZE];
 
 int _printf(const char *format, ...);
+void write_char(char c, int *n);
+void write_string(char *str, int *n);
+void write_percent(int *n);
+void write_int(int arg, int *n);
+void write_uint(uint32_t arg, int *n, int base, int is_uppercase);
+void write_pointer(void *arg, int *n);
+void write_string_special(char *str, int *n);
 
 #endif /* MAIN_H */
